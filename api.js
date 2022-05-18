1af2f49898b4e462e616db15d1242231b5c7709c
Roshu2 <kyumin1020@gmail.com>
2022-05-18 22:22:07 +0900

    1. 거북이반 4강 게시물 게시

--- api.js
+++ api.js
@@ -14,10 +14,10 @@ async function handleSignin(){
         body:JSON.stringify(signupData)
     }
     )
-    console.log(response)
+    
 
     response_json = await response.json()
-    console.log(response_json)
+    
 
     if (response.status == 200){
         window.location.replace(`${frontend_base_url}/login.html`);
@@ -41,7 +41,7 @@ async function handleLogin(){
     }
     )
     response_json = await response.json()
-    console.log(response_json)
+    
     localStorage.setItem("token", response_json.token)
 
     if (response.status == 200){
@@ -51,11 +51,9 @@ async function handleLogin(){
     }
 }
 
+// 이름 불러오는 함수
 async function getName(){
-    console.log("get Name")
-    console.log(localStorage.getItem("token"))
-
-    const response = await fetch(`${backend_base_url}/getuserinfo`,{
+    const response = await fetch(`${backend_base_url}/getuserinfo`, {
         headers:{
             'Authorization' : localStorage.getItem("token")
         }
@@ -63,12 +61,48 @@ async function getName(){
     }
     )
     response_json = await response.json()
-    console.log(response_json)
 
     const username = document.getElementById("username")
     username.innerText = response_json.email
 
-
-    
+    // return response_json.email
 }
 
+
+
+async function postArticle(title, content){
+    // 입력값 받아온다
+    const articleData = {
+        title : title,
+        content : content,
+    }
+    // api 서버통신
+    const response = await fetch(`${backend_base_url}/article`, {
+        method: 'POST',
+        headers:{
+            'Authorization' : localStorage.getItem("token")},
+        body:JSON.stringify(articleData)
+    }
+    )
+    // json형태로 받아온다
+    response_json = await response.json()
+    
+    
+    // 메인페이지로 리다이렉 이동
+    if (response.status == 200){
+        window.location.replace(`${frontend_base_url}/`);
+    }else {
+        alert(response.status)
+    }
+
+}
+
+async function getArticles(){
+    const response = await fetch(`${backend_base_url}/article`, {
+        method:'GET',
+    }
+    )
+    // json형태로 받아온다
+    response_json = await response.json()
+    return response_json.articles
+}
